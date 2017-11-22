976ded0e0717e7f1e9eaed5d42306ee3a262cb85
Lucas Stoque <stoque@live.com>
2017-11-21 23:32:09 -0200

    Finalizado challenge-26

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -20,12 +20,43 @@ Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
 // ?
+(function() {
+  'use strict';
+  
+  function DOM(elements) {
+    this.elements = document.querySelectorAll(elements);
+  }
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+  DOM.prototype.on = function on(event, callback) {
+    Array.prototype.forEach.call(this.elements, function(element) {
+      element.addEventListener(event, callback, false);
+    })
+  }
+  
+  DOM.prototype.off = function off(event, callback) {
+    Array.prototype.forEach.call(this.elements, function(element) {
+      element.removeEventListener(event, callback, false);
+    })
+  }
+  
+  DOM.prototype.get = function get() {
+      return this.elements;
+  }
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+  var $a = new DOM('[data-js="link"]');
+  var $btn = new DOM('[data-js="button"]');
+
+  $btn.on('click', handleClick);
+  $a.on('click', handleClick);
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('clicou');
+  }
+
+  console.log($btn.get());
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.querySelector('.box'));
+  console.log('$btn é filho de body?', $btn.get()[0].parentNode === document.querySelector('.box'));
+})();
